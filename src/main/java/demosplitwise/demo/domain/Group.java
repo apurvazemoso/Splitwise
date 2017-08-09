af7a3692e88bd9aba6ebb9f76b4048c2ab2113bd
prannoy29 <prannoysarkar1993@gmail.com>
2017-08-09 12:18:10 +0530

    added all except user properties and group controller

--- src/main/java/demosplitwise/demo/domain/Group.java
+++ src/main/java/demosplitwise/demo/domain/Group.java
@@ -1,10 +1,14 @@
 package demosplitwise.demo.domain;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Date;
 
 @Entity
-@Table(name="group")
+@Table(name="splitwisegroup")
 public class Group implements Serializable {
     public static final long serialVersionUID = -3109157732242239406L;
 
@@ -13,25 +17,73 @@ public class Group implements Serializable {
     @Column(name = "group_id")
     private long groupId;
 
+    public long getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(long groupId) {
+        this.groupId = groupId;
+    }
+
     @Column(name = "group_name")
-    String groupName;
+    private String groupName;
 
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+
+    @Column(name = "date_of_creation",updatable = false)
+    private Date dateOfCreation = new Date();
+
+    public Date getDateOfCreation() {
+        return dateOfCreation;
+    }
+
+    //public void setDateOfCreation(Date dateOfCreation) {this.dateOfCreation = dateOfCreation;}
+
+
+    @Column(name = "date_of_update")
+    private Date dateOfUpdate= new Date();
+
+    @PreUpdate
+    public void setLastUpdate() {  this.dateOfUpdate = new Date();}
+    
     @Column(name = "Total_Members")
-    int totalMembers;
+    private int totalMembers;
 
-    @Column(name = "Created_by")
-    String createdBy;
+    public int getTotalMembers() {
+        return totalMembers;
+    }
 
-    @Column(name = "date_of_creation")
-    String dateOfCreation;
+    public void setTotalMembers(int totalMembers) {
+        this.totalMembers = totalMembers;
+    }
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+
 
     protected Group() {
     }
 
-    public Group(String groupName, String createdBy, String dateOfCreation) {
+    public Group(String groupName, String createdBy) {
         this.groupName = groupName;
         this.createdBy = createdBy;
-        this.dateOfCreation = dateOfCreation;
     }
 
     @Override
